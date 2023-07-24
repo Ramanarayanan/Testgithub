9f926e230773e54b7457442ba3d17e2d822b3485
Ramanarayanan K <RAMANARAYANANK.IN@GMAIL.COM>
2023-07-24 13:27:20 +0530

    Create check_status.sh

---
+++ .github/workflows/check_status.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Function to check the status of a GitHub Actions job
+check_job_status() {
+  job_status=$(curl -s -H "Authorization: Bearer $GITHUB_TOKEN" \
+    "https://api.github.com/repos/$GITHUB_REPOSITORY/actions/jobs/$1" | jq -r '.conclusion')
+  echo "$job_status"
+}
+
+# Array containing all job names
+job_names=("job1" "job2" "job3") # Add other job names as needed
+
+# Loop until all jobs have succeeded
+while true; do
+  all_succeeded=true
+
+  for job_name in "${job_names[@]}"; do
+    job_id=$(curl -s -H "Authorization: Bearer $GITHUB_TOKEN" \
+      "https://api.github.com/repos/$GITHUB_REPOSITORY/actions/workflows/$GITHUB_WORKFLOW/runs" | jq -r --arg job_name "$job_name" '.workflow_runs[] | select(.name == $job_name) | .id')
+
+    job_status=$(check_job_status "$job_id")
+
+    if [ "$job_status" != "success" ]; then
+      all_succeeded=false
+      break
+    fi
+  done
+
+  if [ "$all_succeeded" = true ]; then
+    break
+  fi
+
+  echo "Not all jobs have succeeded yet. Waiting..."
+  sleep 30
+done
+
+echo "All jobs have succeeded!"

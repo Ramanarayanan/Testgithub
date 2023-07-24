d1ab521b4ab679567f26f926a2a8e2d745a9d896
Ramanarayanan K <RAMANARAYANANK.IN@GMAIL.COM>
2023-07-24 14:28:19 +0530

    Update check_status.sh

--- .github/workflows/check_status.sh
+++ .github/workflows/check_status.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-# Function to check the status of a GitHub Actions job
-check_job_status() {
-  job_status=$(curl -s -H "Authorization: Bearer $GITHUB_TOKEN" \
-    "https://api.github.com/repos/$GITHUB_REPOSITORY/actions/jobs/$1" | jq -r '.conclusion')
-  echo "$job_status"
+# Function to check the status of a GitHub Actions workflow
+check_workflow_status() {
+  workflow_status=$(curl -s -H "Authorization: Bearer $GITHUB_TOKEN" \
+    "https://api.github.com/repos/$GITHUB_REPOSITORY/actions/runs/$1" | jq -r '.conclusion')
+  echo "$workflow_status"
 }
 
-# Array containing all job names
-job_names=("Build_NUnitTests" "Build_Manual" "New_Build_Deployment") 
+# Array containing all dependent workflow names
+workflow_names=("workflow1.yml" "workflow2.yml") # Add other workflow names as needed
 
-# Loop until all jobs have succeeded
+# Loop until all dependent workflows have succeeded
 while true; do
   all_succeeded=true
 
-  for job_name in "${job_names[@]}"; do
-    job_id=$(curl -s -H "Authorization: Bearer $GITHUB_TOKEN" \
-      "https://api.github.com/repos/$GITHUB_REPOSITORY/actions/workflows/$GITHUB_WORKFLOW/runs" | jq -r --arg job_name "$job_name" '.workflow_runs[] | select(.name == $job_name) | .id')
+  for workflow_name in "${workflow_names[@]}"; do
+    workflow_id=$(curl -s -H "Authorization: Bearer $GITHUB_TOKEN" \
+      "https://api.github.com/repos/$GITHUB_REPOSITORY/actions/workflows" | jq -r --arg workflow_name "$workflow_name" '.workflows[] | select(.path == $workflow_name) | .id')
 
-    job_status=$(check_job_status "$job_id")
+    workflow_status=$(check_workflow_status "$workflow_id")
 
-    if [ "$job_status" != "success" ]; then
+    if [ "$workflow_status" != "success" ]; then
       all_succeeded=false
       break
     fi
@@ -30,8 +30,8 @@ while true; do
     break
   fi
 
-  echo "Not all jobs have succeeded yet. Waiting..."
+  echo "Not all dependent workflows have succeeded yet. Waiting..."
   sleep 30
 done
 
-echo "All jobs have succeeded!"
+echo "All dependent workflows have succeeded!"
